40ced297fd259b21d0959b35e63faf1ce36e0f95
Denzelf-png <79867017+Denzelf-png@users.noreply.github.com>
2021-05-07 10:03:22 -0700

    Add files via upload

--- tKinterTest.py
+++ tKinterTest.py
@@ -15,21 +15,43 @@ def exportList():
     with open('test.txt', 'w') as f:
         for item in playlist:
             f.write("%s\n" % item)
+
+
+def clearWindow():
+    for widgets in top.winfo_children():
+        widgets.destroy()
+
+def mainMenu():
+    clearWindow()
+    LMain = Label(top, text = "Block 5 GUI Projects")
+    Lmain.grid(column = 0, row = 1)
+    B1Main = Button(text = "Week 1", bg = "blue")
+    B1Main.grid(column = 0, row = 2)
+    B2Main = Button(text = "Week 2", bg = "blue")
+    B2Mian.grid(column = 0, row = 3)
+    B3Main = Button(text = "Week 3", bg = "blue")
+    B3Main.grid(column = 0, row = 4)
     
 
 # This is a Label widget
 L1 = Label(top, text="Playlist Generator")
-L1.grid(column= 0, row= 1)
+L1.grid(column= 0, row = 1)
 
 # This is an Entry widget
 E1 = Entry(top, bd = 5)
-E1.grid(column= 0, row= 2)
+E1.grid(column= 0, row = 2)
 
 #This is a Button widget
-B1 = Button(text= " +  ", bg="purple", command= results)
-B1.grid(column= 1, row= 2)
+B1 = Button(text= " +  ", bg = "green", command = results)
+B1.grid(column = 1, row = 2)
 
-B2 = Button(text= " Print ", bg = "light blue", command= printList)
-B2.grid(column= 2, row= 2) 
+B2 = Button(text = " Print ", bg = "light blue", command = printList)
+B2.grid(column = 2, row = 2) 
+
+B3 = Button(text = "Export List", bg = "pink", command = exportList)
+B3.grid(column = 0, row = 3)
+
+Bexit = Button(text = "Clear Window", bg = "red", command = clearWindow)
+Bexit.grid(column = 1, row = 3)
 
 top.mainloop()
