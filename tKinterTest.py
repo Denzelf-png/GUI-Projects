ccd0abb057ac6d314e243f0c178244819cd0a47d
Denzelf-png <79867017+Denzelf-png@users.noreply.github.com>
2021-05-11 10:02:44 -0700

    Add files via upload

--- tKinterTest.py
+++ tKinterTest.py
@@ -3,11 +3,7 @@ from tkinter import *
 top = Tk()
 playlist = []
 
-def results():
-    result = E1.get()
-    playlist.append(result)
-    E1.delete(0, END)
-    
+   
 def printList():
     print(playlist)
 
@@ -24,34 +20,56 @@ def clearWindow():
 def mainMenu():
     clearWindow()
     LMain = Label(top, text = "Block 5 GUI Projects")
-    Lmain.grid(column = 0, row = 1)
-    B1Main = Button(text = "Week 1", bg = "blue")
+    LMain.grid(column = 0, row = 1)
+    B1Main = Button(text = "Week 1", bg = "blue", command = week1)
     B1Main.grid(column = 0, row = 2)
     B2Main = Button(text = "Week 2", bg = "blue")
-    B2Mian.grid(column = 0, row = 3)
+    B2Main.grid(column = 0, row = 3)
     B3Main = Button(text = "Week 3", bg = "blue")
     B3Main.grid(column = 0, row = 4)
     
+def week1():
+    clearWindow()
+    def results():
+        result = E1.get()
+        playlist.append(result)
+        E1.delete(0, END)
+        
+    # This is a Label widget
+    L1 = Label(top, text="Playlist Generator")
+    L1.grid(column= 0, row = 1)
 
-# This is a Label widget
-L1 = Label(top, text="Playlist Generator")
-L1.grid(column= 0, row = 1)
+    # This is an Entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column= 0, row = 2)
 
-# This is an Entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column= 0, row = 2)
+    #This is a Button widget
+    B1 = Button(text= " +  ", bg = "green", command = results)
+    B1.grid(column = 1, row = 2)
 
-#This is a Button widget
-B1 = Button(text= " +  ", bg = "green", command = results)
-B1.grid(column = 1, row = 2)
+    B2 = Button(text = " Print ", bg = "light blue", command = printList)
+    B2.grid(column = 2, row = 2) 
 
-B2 = Button(text = " Print ", bg = "light blue", command = printList)
-B2.grid(column = 2, row = 2) 
+    B3 = Button(text = "Export List", bg = "pink", command = exportList)
+    B3.grid(column = 0, row = 3)
 
-B3 = Button(text = "Export List", bg = "pink", command = exportList)
-B3.grid(column = 0, row = 3)
+    Bexit = Button(text = "Clear Window", bg = "red", command = mainMenu)
+    Bexit.grid(column = 1, row = 3)
 
-Bexit = Button(text = "Clear Window", bg = "red", command = clearWindow)
-Bexit.grid(column = 1, row = 3)
 
-top.mainloop()
+def week2():
+    clearWindow()
+    B1Week2 = Button()
+    L1Week2 = Label()
+    L2Week2 = Label()
+    L3Week2 = Label()
+    E1Week2 = Entry()
+    E2Week2 = Entry()
+
+
+
+
+
+if __name__ == "__main__":
+    mainMenu()
+    top.mainloop()
